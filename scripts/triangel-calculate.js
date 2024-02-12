b591990c486eeb53b1f6dde6735b7c86e8abc316
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-02-12 22:57:43 +0600

    Added js file for triangle

---
+++ scripts/triangel-calculate.js
@@ -0,0 +1,7 @@
+function calculateTriangleArea(){
+    // Get Triangle Basr value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    // console.log(base)
+}
